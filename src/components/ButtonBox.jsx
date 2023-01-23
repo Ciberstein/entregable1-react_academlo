4892367e7b46fc012c73d2dedf2fdba3a1399713
cerberus8888 <v26396341@gmail.com>
2023-01-23 15:53:43 -0500

    Update

--- src/components/ButtonBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ButtonBox = ({ handleClick, colorRand }) => {
-  return (
-    <button onClick={handleClick} className='box__btn' style={{ backgroundColor: colorRand }}><i className='fas fa-chevron-right'></i></button>
-  )
-}
